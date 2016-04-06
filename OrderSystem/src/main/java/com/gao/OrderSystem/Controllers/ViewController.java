4350767673a59cc1e6d66a94ef2f01d1b93a53b6
王齐源 <wqy813853090@live.cn>
2016-04-06 17:30:53 +0800

    修改

---
+++ OrderSystem/src/main/java/com/gao/OrderSystem/Controllers/ViewController.java
@@ -0,0 +1,14 @@
+package com.gao.OrderSystem.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping(value="login.action")
+	public String test() {
+		return "test";
+	}
+	
+}
